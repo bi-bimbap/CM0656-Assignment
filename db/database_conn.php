bc435599ef5c2d0ea6700e72cb4bf2c7498be4e8
Seah Jia-Min <seahjm96040207@gmail.com>
2017-10-28 20:58:45 +0800

    Updated database name

---
+++ db/database_conn.php
@@ -0,0 +1,25 @@
+<?php
+  // Set up connection to database
+  define ('SERVER', 'localhost');
+  define ('USER', 'root');
+  define ('PASSWORD','');
+  define ('DATABASE', 'ima_megastar');
+
+  $conn = mysqli_connect(SERVER, USER, PASSWORD, DATABASE);
+  if (mysqli_connect_errno()) {
+  	echo "<p>Connection failed:".mysqli_connect_error()."</p>\n"; //Test
+  }
+?>
+
+<?php
+	//Set up connection to database
+//   define ('SERVER', 'localhost');
+//   define ('USER', 'SE209');
+//   define ('PASSWORD','902ES');
+//   define ('DATABASE', 'SE209');
+//
+//   $conn = mysqli_connect(SERVER, USER, PASSWORD, DATABASE);
+//   if (mysqli_connect_errno()) {
+//   echo "<p>Connection failed:".mysqli_connect_error()."</p>\n";
+// }
+?>
