701dccba688de4773efab3fb03e8d535fa3249fc
Seah Jia-Min <seahjm96040207@gmail.com>
2017-11-03 20:49:59 +0800

    Added constants for easy switching between localhost & server

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+//https://stackoverflow.com/questions/14371599/how-to-implement-absolute-urls-on-localhost-and-web-server
+define("LOCAL", "http://localhost");
+//define("WEB", "http://foo.bar");
+?>
