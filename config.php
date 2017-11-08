1fa300555d8f977b3336be2423cca16c121f303d
Seah Jia-Min <seahjm96040207@gmail.com>
2017-11-08 09:18:39 +0800

    Updated static path for WEB

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 //https://stackoverflow.com/questions/14371599/how-to-implement-absolute-urls-on-localhost-and-web-server
 define("LOCAL", "http://localhost");
-//define("WEB", "http://foo.bar");
+define("WEB", "http://student.kdupg.edu.my/ima-fanbase");
 ?>
