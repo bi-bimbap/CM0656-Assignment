3462121daab032c311e04e945abf776b31be56fc
yqcheong <yqcheong@github.com>
2017-11-14 17:31:04 +0800

    Add New Thread data into database

--- discussion/Admin_create_new_thread.php
+++ discussion/Admin_create_new_thread.php
@@ -1,62 +1,84 @@
 <?php
 ini_set("session.save_path", "");
-session_start(); //start session
-include '../db/database_conn.php'; //include databas
+session_start();
+include '../db/database_conn.php';
 include_once '../config.php';
 require_once('../controls.php');
 require_once('../functions.php');
 echo makePageStart("Create Discussion Thread");
-echo makeProfileButton();
 echo makeNavMenu();
 echo makeHeader("Create Discussion Thread");
-$environment = LOCAL;
+$environment = LOCAL; //TODO: Change to server
 ?>
 
-<link href="../css/bootstrap.css" rel="stylesheet">
+<!-- CSS style -->
+<link rel='stylesheet' href='../css/bootstrap.css' />
+<link rel="stylesheet" href="../css/jquery-ui.min.css" />
+<link rel="stylesheet" href="../css/jquery-ui.dataTables.min.css" />
+<link rel="stylesheet" href="../css/parsley.css" />
+<link rel="stylesheet" href="../css/stylesheet.css" />
+
+<script src='../scripts/-bootstrap.js'></script>
+<script src='../scripts/jquery-ui.min.js'></script>
+<script src='../scripts/jquery-ui.dataTables.min.js'></script>
 <script src="../scripts/jquery.js"></script>
-<script src="../scripts/bootstrap.min.js"></script>
+<script src="../scripts/parsley.min.js"></script>
 
-<!-----------------------------------------------------------
-    Verification - Only Admin Allowed to Login
------------------------------------------------------------->
+<!-- TODO: Verification - Only Admin Allowed to Login -->
 
-
-  <!-----------------------------------------------------------
-      DISCUSSION BOARD : Create New Thread
-  ------------------------------------------------------------>
-  <h2>Popup</h2>
-
-  <div class="box-wrapper">
-    <form method="post" action="create_new_thread.php">
-          <div class="form-group">
-            <label for="thread_name" >Thread Name</label>
-              <input type="text" class="form-control" id="thread_name" name="thread_name" placeholder="Thread Name"</>
-            </br>
-            <label for="thread_desc" >Thread Description</label>
-              <input type="text" class="form-control" id="thread_desc" name="thread_desc" placeholder="Description">
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="form-group">
-          <div class="col-sm-3 col-md-offset-9">
-            <button class ="btn btn-block btn-success btn-lg" type="submit" name="Submit" id="createThread_submit" onClick="createThreadForm()" VALUE="Submit">Create</button>
-          </div>
-        </div>
-      </div>
-<!-----------------------------------------------------------
-   Script - Pop Our Form for Create New Thread
------------------------------------------------------------->
-      <script>
-      // When the user clicks on div, open the popup
-      function createThreadForm() {
-          var popup = document.getElementById("createThread_submit");
-          popup.classList.toggle("show");
-      }
-      </script>
+<!------------------------------------------------------------------------------------------------------------------------
+      DISCUSSION BOARD : Create New Thread Form
+------------------------------------------------------------------------------------------------------------------------->
+    <form id="createThreadForm" data-parsley-validate method="post">
+          <p>Thread Name: <input type="text" id="txtThreadName" name="txtThreadName" placeholder="Thread Name" /></p>
+          <p>Thread Description: <input type="text" id="txtThreadDesc" name="txtThreadDesc" placeholder="Description" /></p>
+          <!-- <input type="submit" name="createThread_submit" id="createThread_submit" value="Create" /> -->
+          <button type="submit" name="createThread_submit" id="createThread_submit">Create</button>
+      </br>
+      </br>
+      </br>
     </form>
 
+<!-- TODO: Change the form to be POP OUT form -->
+<!--------------------------------------------------------------------------------------------------------------------------
+   Script - Pop Our Form for Create New Thread
+--------------------------------------------------------------------------------------------------------------------------->
+
+<!--------------------------------------------------------------------------------------------------------------------------
+      DISCUSSION BOARD: "Create" Submit Button Function
+--------------------------------------------------------------------------------------------------------------------------->
 <?php
-//echo makeFooter();
+      if(isset($_POST['createThread_submit'])){
+        //obtain user input
+        $thread_name = filter_has_var(INPUT_POST,'txtThreadName') ? $_POST['txtThreadName']: null;
+        $thread_desc = filter_has_var(INPUT_POST,'txtThreadDesc') ? $_POST['txtThreadDesc']: null;
+
+        //Trim white space
+        $thread_name = trim($thread_name);
+        $thread_desc = trim($thread_desc);
+
+        //Sanitize user input
+        $thread_name = filter_var($thread_name, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+        $thread_desc = filter_var($thread_desc, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+        $sqlNewThread = "INSERT INTO discussion_thread (threadName, threadDescription)	VALUES (?,?)";
+        $stmtNewThread = mysqli_prepare($conn, $sqlNewThread) or die( mysqli_error($conn));
+        mysqli_stmt_bind_param($stmtNewThread, 'ss', $thread_name, $thread_desc);
+        mysqli_stmt_execute($stmtNewThread);
+        //d = double; i = integer; s = string
+
+      	if (mysqli_stmt_affected_rows($stmtNewThread) > 0) {
+      		echo "<script>alert('New Thread has been created successfully!')</script>";
+      	}
+        else {
+      		echo "<script>alert('Failed to create! Try Again!')</script>";
+      	}
+        mysqli_stmt_close($stmtNewThread);
+        mysqli_close($conn);
+}
+?>
+
+<?php
+echo makeFooter();
 echo makePageEnd();
 ?>
