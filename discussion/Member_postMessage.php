51e51d8f9c3ad831f6e58e635a4f812a3697ef98
yqcheong <yqcheong@github.com>
2017-11-19 13:25:40 +0800

    Done Post Message, Left "Reply Msg" and "Report Inappro"

--- discussion/Member_postMessage.php
+++ discussion/Member_postMessage.php
@@ -50,7 +50,7 @@ $environment = LOCAL; //TODO: Change to server
     <div class='container'>
         <div class='message-group'>
 <!--*******************************************************************************************************************************************************
-      DISCUSSION BOARD: Display Message Content
+      DISCUSSION BOARD: (1) Display Message Content
 *******************************************************************************************************************************************************-->
         <?php
             $sqlGetMessage = "SELECT user.username, discussion_message.messageContent, discussion_message.messageDateTime
@@ -67,45 +67,60 @@ $environment = LOCAL; //TODO: Change to server
             mysqli_stmt_execute($stmtGetMessage);
             mysqli_stmt_bind_result($stmtGetMessage, $userName, $messageContent, $messageDateTime);
 
-            while(mysqli_stmt_fetch($stmtGetMessage)){
-                echo"
-                  <div class='message-created'>
-                      <p><h6>$userName</h6></p>
-                      <p><h6>$messageDateTime</h6></p>
-                      <p><h4>$messageContent</h4></p>
-                      <p>
-                        <a>Reply</a>
-                      </p>
-                    </div>
-                    </br>";
+              //if(mysqli_stmt_num_rows($stmtGetMessage) == 0){
 
-            }
+              if(mysqli_stmt_affected_rows($stmtGetMessage) < 0 ){
+
+                //calculate total messages in a thread
+                $messageNum = 0;
+                while(mysqli_stmt_fetch($stmtGetMessage)){
+                  echo"
+                    <div class='message-created'>
+                        <p><h6>$userName</h6></p>
+                        <p><h6>$messageDateTime</h6></p>
+                        <p><h4>$messageContent</h4></p>";
+
+                        /*************************************************************************************************************************************************
+                              DISCUSSION BOARD: (2) Reply Message (Member)
+                        *************************************************************************************************************************************************/
+                        //Validation - Only Only member can view "Reply" button
+                        if((isset($_SESSION['logged-in']) && $_SESSION['logged-in'] == true) && (isset($_SESSION['userID'])) &&
+                        (isset($_SESSION['userType']) && ($_SESSION['userType'] == "junior" || $_SESSION['userType'] == "senior"))) {
+                            //echo "<p><a>Reply</a></p>";
+                            echo "<p><h6><button type='submit' id='replyMessage_submit' name='replyMessage_submit' />Reply</h6></p>
+                            </div>
+                            </br>";
+                        }
+                    $messageNum++;
+                  }
+
+                  if($messageNum == 0){
+                    echo"
+                      <div class='message-created'>
+                          <p><h4>No Message Content</h4></p>
+                      </div>
+                      </br>";
+                  }
+              }
+            //}
+
             mysqli_stmt_close($stmtGetMessage);
         ?>
 
         <?php
-        /*******************************************************************************************************************************************************
-              DISCUSSION BOARD: Reply Message (Member)
-        *******************************************************************************************************************************************************/
-            //TODO Validation: Only Member can see This
-            //Validation - Only member can reply message
-              // echo"
-              //         <p>
-              //           <a>Reply</a>
-              //         </p>
-              //       </div>
-              //       </br>";
-        ?>
+            //Only show content if user is logged in
+            $_SESSION['userID'] = '1'; //TODO: Remove session
+            $_SESSION['userType'] = 'senior'; //TODO: Remove
+            $_SESSION['username'] = 'seahjm'; //TODO: Remove
+            $_SESSION['logged-in'] = true; //TODO: Remove
 
-        <?php
-            if((isset($_SESSION['logged-in']) && $_SESSION['logged-in'] == true) &&
+            //Validation - Only Only member can view "Post" textfield
+            if((isset($_SESSION['logged-in']) && $_SESSION['logged-in'] == true) && (isset($_SESSION['userID'])) &&
             (isset($_SESSION['userType']) && ($_SESSION['userType'] == "junior" || $_SESSION['userType'] == "senior"))) {
 
               /*******************************************************************************************************************************************************
-                    DISCUSSION BOARD: Post New Message (Member)
+                    DISCUSSION BOARD: (3) Post New Message (Member)
               *******************************************************************************************************************************************************/
-              //TODO Validation: Only Member can see This
-              //Validation - Only member can post message
 
               if(isset($_POST['postMessage_submit']) && !empty($_POST['txtPostMessage'])){
                 //obtain user input
@@ -117,18 +132,12 @@ $environment = LOCAL; //TODO: Change to server
                 //Sanitize user input
                 $post_message = filter_var($post_message, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
-                //TODO get userID
-                //TODO get messageStatus
-                // if(isset($_GET['userID'])){
-                //   $threadID = $_GET['userID'];
-                // }
-
                 //Insert user's input into database
-                $sqlPostMessage = "INSERT INTO discussion_message (messageContent)	VALUES (?)";
+                $sqlPostMessage = "INSERT INTO discussion_message (userID, messageContent)	VALUES (?,?)";
                 $stmtPostMessage = mysqli_prepare($conn, $sqlPostMessage) or die( mysqli_error($conn));
-                mysqli_stmt_bind_param($stmtPostMessage, 's', $messageContent);
+                mysqli_stmt_bind_param($stmtPostMessage, 'ss', $_SESSION['userID'], $post_message);
                 mysqli_stmt_execute($stmtPostMessage);
-                mysqli_stmt_bind_result($stmtPostMessage, $messageContent);
+                //mysqli_stmt_bind_result($stmtPostMessage, $_SESSION['userID'], $post_message);
 
                 if(mysqli_stmt_affected_rows($stmtPostMessage) > 0){
                   echo "<script>alert('The message has been posted')</script>";
@@ -136,25 +145,27 @@ $environment = LOCAL; //TODO: Change to server
                 else {
                   echo "<script>alert('Try again!')</script>";
                 }
+                //validation: Prevent Resubmit Users' Previous Input Data
+                clearstatcache();
                 mysqli_stmt_close($stmtPostMessage);
               }
-
-          }
-          else {
-            $url = "../loginForm.php";
-            echo "<script type='text/javascript'>";
-            echo "alert('You are not member, please log in as member to post message!');";
-            echo 'window.location.href="'.$url.'";';
-            echo "</script>";
-          }
+              echo"
+                  <div class='message-new'>
+                    <form id='postMessage_field' method='post'>
+                      <input type='text' id='txtPostMessage' name='txtPostMessage' data-parsley-required='true' placeholder='Post a message..' />
+                      <input type='submit' id='postMessage_submit' name='postMessage_submit' value='Post'/>
+                    </form>
+                  </div>";
+            }
+            else {
+              echo"
+                  <div class='message-new'>
+                    <form id='postMessage_field' method='post'>
+                    </form>
+                  </div>";
+            }
         ?>
         <!-- onKeyPress='postMessage(event)' -->
-            <div class='message-new'>
-              <form id='postMessage_field' method='post'>
-                <input type='text' id='txtPostMessage' name='txtPostMessage' data-parsley-required='true' placeholder='Post a message..' />
-                <input type='submit' id='postMessage_submit' name='postMessage_submit' value='Post'/>
-              </form>
-            </div>
       </div>
   </div>
 </div>
@@ -163,8 +174,13 @@ $environment = LOCAL; //TODO: Change to server
 <!--*******************************************************************************************************************************************************
       DISCUSSION BOARD: Report Inappropriate Message (Member)
 *******************************************************************************************************************************************************-->
-    <!-- Validation - Only member can report inappropriate message -->
-
+<?php
+                  // echo"
+                  //     <div class='message-new'>
+                  //       <form id='postMessage_field' method='post'>
+                  //       </form>
+                  //     </div>";
+?>
 
 <?php
             mysqli_close($conn);
