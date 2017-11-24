cad7f649136f71adec39f14aefedbf2c1c6f6483
zhch96 <33235850+zhch96@users.noreply.github.com>
2017-11-24 18:49:41 +0800

    Update Admin_userManagement.php

--- administration/Admin_userManagement.php
+++ administration/Admin_userManagement.php
@@ -8,10 +8,16 @@ include_once '../config.php';
 require_once('../controls.php');
 require_once('../functions.php');
 echo makePageStart("User Management");
+	echo makeWrapper();
 echo "<form method='post'>" . makeLoginLogoutBtn("../") . "</form>";
 echo makeProfileButton("../");
 echo makeNavMenu("../");
 echo makeHeader("User Management");
+
+
+
+
+
 $environment = WEB; //TODO: change to server
 ?>
 
@@ -20,7 +26,19 @@ $environment = WEB; //TODO: change to server
 <script src="../scripts/jquery.js"></script>
 <script src="../scripts/bootstrap.min.js"></script>
 <script src="../scripts/jquery.dataTables.min.js"></script>
+<script src="scripts/jquery.js"></script>
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i" rel="stylesheet">
+<script src="scripts/parsley.min.js"></script>
+<link rel="stylesheet" href="../css/parsley.css" type="text/css" />
+<link rel="stylesheet" href="../css/stylesheet.css" type="text/css" />
+<link href="../css/bootstrap.css" rel="stylesheet">
+<script src="scripts/bootstrap.min.js"></script>
 
+	<div class="content">
+		<div class="container">
 <?php //Only show content to admin/main admin
 // $_SESSION['userID'] = '3'; //TODO: Remove
 // $_SESSION['userType'] = 'mainAdmin'; //TODO: Remove
@@ -511,7 +529,8 @@ else { //Redirect user to home page
   header("Refresh:1;url=../index.php");
 }
 ?>
-
+</div>
+</div>
 <script src="../scripts/parsley.min.js"></script>
 <link rel="stylesheet" href="../css/parsley.css" type="text/css" />
 
