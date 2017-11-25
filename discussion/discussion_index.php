4b629ab3e9da282df86da567c907cc5ac53d0834
yqcheong <yqcheong@github.com>
2017-11-25 17:41:03 +0800

    minor changes.. function remain

--- discussion/discussion_index.php
+++ discussion/discussion_index.php
@@ -6,10 +6,10 @@ require_once('../config.php');
 require_once('../controls.php');
 require_once('../functions.php');
 echo makePageStart("Discussion Index");
-echo makeWrapper();
-echo "<form method='post'>" . makeLoginLogoutBtn() . "</form>";
-echo makeProfileButton();
-echo makeNavMenu();
+echo makeWrapper("");
+echo "<form method='post'>" . makeLoginLogoutBtn("") . "</form>";
+echo makeProfileButton("");
+echo makeNavMenu("");
 echo makeHeader("Discussion Index");
 $environment = LOCAL;
 ?>
@@ -117,113 +117,6 @@ $environment = LOCAL;
     // mysqli_stmt_close($stmtDiscussion);
     mysqli_close($conn);
 //} //for ajax, close
-echo makeFooter();
-echo makePageEnd();
-?>
-=======
-<?php
-ini_set("session.save_path", "");
-session_start();
-include '../db/database_conn.php';
-include_once '../config.php';
-require_once('../controls.php');
-require_once('../functions.php');
-echo makePageStart("Discussion Index");
-echo makeWrapper();
-echo "<form method='post'>" . makeLoginLogoutBtn() . "</form>";
-echo makeProfileButton();
-echo makeNavMenu();
-echo makeHeader("Discussion Index");
-$environment = LOCAL;
-?>
-
-<!-- CSS style -->
-<link rel='stylesheet' href='../css/bootstrap.css' />
-<link rel="stylesheet" href="../css/jquery-ui.min.css" />
-<link rel="stylesheet" href="../css/jquery.dataTables.min.css" />
-<link rel="stylesheet" href="../css/parsley.css" />
-<link rel="stylesheet" href="../css/stylesheet.css" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,400i,700">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i">
-<script src='../jquery-2.2.0.js'></script>
-<script src="../jquery-ui.js"></script>
-<script src="../scripts/bootstrap.min.js"></script>
-<script src="../scripts/parsley.min.js"></script>
-<script src="../scripts/jquery.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<!--******************************************************************************************************************
-    Validation - Only Only admin can view "Create New Thread" button
-******************************************************************************************************************-->
-<?php
-
-  if((isset($_SESSION['logged-in']) && $_SESSION['logged-in'] == true) &&
-  (isset($_SESSION['userType']) && ($_SESSION['userType'] == "admin" || $_SESSION['userType'] == "mainAdmin"))) {
-    echo
-    "<form method='post' action='Admin_createThread.php'>
-      <input type='submit' value='Create New Thread' name='createThread_submit' />
-    </form>";
-  }
-?>
-<!--******************************************************************************************************************
-    DISCUSSION BOARD : List Created Discussion Message and Information
-*******************************************************************************************************************-->
-<div class="displayDiscussionInfo">
-<table id="tblThreadList" class="display" cellspacing="0" width="100%">
-  <thead>
-    <tr>
-      <th>Thread Name</th>
-      <th>Created By</th>
-      <!-- <th>Total Message</th> -->
-    </tr>
-  </thead>
-
-  <?php
-      $sqlDiscussion = "SELECT discussion_thread.threadID, discussion_thread.threadName, user.username
-      FROM discussion_thread
-      INNER JOIN discussion_message
-      ON discussion_thread.threadID=discussion_message.threadID
-      INNER JOIN user
-      ON discussion_thread.userID=user.userID
-      ORDER BY discussion_thread.threadID";
-      $stmtDiscussion = mysqli_prepare($conn, $sqlDiscussion) or die( mysqli_error($conn));
-      mysqli_stmt_execute($stmtDiscussion);
-      mysqli_stmt_bind_result($stmtDiscussion, $thread_id, $thread_name, $admin_username);
-      while (mysqli_stmt_fetch($stmtDiscussion)) {
-          echo
-          "<tbody>
-              <tr>
-                <td><a href=\"Member_postMessage.php?threadID=$thread_id\">$thread_name</a></td>
-                <td>$admin_username</td>
-              </tr>
-            </tbody>";
-            //total message
-      }
-  ?>
-
-
-
-<!--***************************************************************************************************************
-    Discussiom: Search Function
-****************************************************************************************************************-->
-
-<!--***************************************************************************************************************
-    Discussiom: Update Function
-****************************************************************************************************************-->
-<!--***************************************************************************************************************
-    Discussiom: Delete Function
-****************************************************************************************************************-->
-</table>
-</div>
-<?php
-    mysqli_stmt_close($stmtDiscussion);
-    mysqli_close($conn);
-//} //for ajax, close
-echo makeFooter();
+echo makeFooter("");
 echo makePageEnd();
 ?>
