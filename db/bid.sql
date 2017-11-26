de63f7c1a2d38d543635227946a7acfbf6159fbc
xeneqn <xene.qn@gmail.com>
2017-11-26 16:30:34 +0800

    Updated auction related tables.

---
+++ db/bid.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 26, 2017 at 09:28 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.0.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ima_megastar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bid`
+--
+
+CREATE TABLE `bid` (
+  `bidID` int(10) NOT NULL,
+  `userID` int(10) NOT NULL,
+  `auctionID` int(10) NOT NULL,
+  `bidAmount` int(10) NOT NULL,
+  `bidStatus` varchar(30) NOT NULL,
+  `bidTime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bid`
+--
+
+INSERT INTO `bid` (`bidID`, `userID`, `auctionID`, `bidAmount`, `bidStatus`, `bidTime`) VALUES
+(1, 1, 1, 55, 'active', '2017-11-12 00:00:00'),
+(2, 42, 1, 60, 'active', '2017-11-12 23:01:22'),
+(3, 46, 1, 100, 'withdrawn', '2017-11-12 23:01:47'),
+(4, 1, 1, 120, 'active', '2017-11-25 23:45:54');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bid`
+--
+ALTER TABLE `bid`
+  ADD PRIMARY KEY (`bidID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bid`
+--
+ALTER TABLE `bid`
+  MODIFY `bidID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
